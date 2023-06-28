50c1c02dd80e149f4caced11f67366f7abfcc0a2
Abel Gómez <abgolla@gmail.com>
2023-06-28 05:16:52 +0200

    Add ellipsis in long filename download links

--- src/themes/sistedes/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/sistedes/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -6,6 +6,7 @@ import { FileSectionComponent as BaseComponent } from '../../../../../../../app/
     selector: 'ds-item-page-file-section',
     templateUrl: './file-section.component.html',
     // templateUrl: '../../../../../../../app/item-page/simple/field-components/file-section/file-section.component.html',
+    styleUrls: ['./file-section.component.scss'],
     animations: [slideSidebarPadding],
 })
 export class FileSectionComponent extends BaseComponent {
--- src/themes/sistedes/app/shared/file-download-link/file-download-link.component.ts
+++ src/themes/sistedes/app/shared/file-download-link/file-download-link.component.ts
@@ -7,8 +7,8 @@ import {
   selector: 'ds-file-download-link',
   // templateUrl: './file-download-link.component.html',
   templateUrl: '../../../../../app/shared/file-download-link/file-download-link.component.html',
-  // styleUrls: ['./file-download-link.component.scss'],
-  styleUrls: ['../../../../../app/shared/file-download-link/file-download-link.component.scss'],
+  styleUrls: ['./file-download-link.component.scss'],
+  // styleUrls: ['../../../../../app/shared/file-download-link/file-download-link.component.scss'],
 })
 export class FileDownloadLinkComponent extends BaseComponent {
 }
